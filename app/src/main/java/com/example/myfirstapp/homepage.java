0530525c181b237d7bb0b7c08f1cda3d27197ed2
EvsChen <chensy1996@sjtu.edu.cn>
2017-10-02 15:49:50 +0800

    Solve conflict

--- app/src/main/java/com/example/myfirstapp/homepage.java
+++ app/src/main/java/com/example/myfirstapp/homepage.java
@@ -1,17 +1,24 @@
 package com.example.myfirstapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class homepage extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private RecyclerView Rv;
+    private ArrayList<HashMap<String,Object>> listItem;
+    private MyAdapter myAdapter;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -25,8 +32,11 @@ public class homepage extends AppCompatActivity {
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_organization:
+                    mTextMessage.setText(R.string.title_organization);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profiles);
                     return true;
             }
             return false;
@@ -38,19 +48,64 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        initData();
+        initView();
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
-        String welcome = "Welcome " + message + "!";
-        textView.setTextSize(40);
-        textView.setText(welcome);
-        ViewGroup layout = (ViewGroup) findViewById(R.id.container);
-        layout.addView(textView);
     }
+    public void initData(){
+        listItem = new ArrayList<HashMap<String, Object>>();/*在数组中存放数据*/
+
+        HashMap<String, Object> map1 = new HashMap<String, Object>();
+        HashMap<String, Object> map2 = new HashMap<String, Object>();
+        HashMap<String, Object> map3 = new HashMap<String, Object>();
+        HashMap<String, Object> map4 = new HashMap<String, Object>();
+        HashMap<String, Object> map5 = new HashMap<String, Object>();
+        HashMap<String, Object> map6 = new HashMap<String, Object>();
+
+        map1.put("ItemTitle", "美国谷歌公司已发出");
+        map1.put("ItemText", "发件人:谷歌 CEO Sundar Pichai");
+        listItem.add(map1);
+
+        map2.put("ItemTitle", "国际顺丰已收入");
+        map2.put("ItemText", "等待中转");
+        listItem.add(map2);
+
+        map3.put("ItemTitle", "国际顺丰转件中");
+        map3.put("ItemText", "下一站中国");
+        listItem.add(map3);
+
+        map4.put("ItemTitle", "中国顺丰已收入");
+        map4.put("ItemText", "下一站广州华南理工大学");
+        listItem.add(map4);
+
+        map5.put("ItemTitle", "中国顺丰派件中");
+        map5.put("ItemText", "等待派件");
+        listItem.add(map5);
+
+        map6.put("ItemTitle", "华南理工大学已签收");
+        map6.put("ItemText", "收件人:Carson");
+        listItem.add(map6);
+    }
+
+    // 绑定数据到RecyclerView
+    public void initView(){
+        Rv = (RecyclerView) findViewById(R.id.my_recycler_view);
+        //使用线性布局
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        Rv.setLayoutManager(layoutManager);
+        Rv.setHasFixedSize(true);
+
+        //用自定义分割线类设置分割线
+        Rv.addItemDecoration(new DividerItemDecoration(this));
+
+        //为ListView绑定适配器
+        myAdapter = new MyAdapter(this,listItem);
+        Rv.setAdapter(myAdapter);
+    }
+
 
 }
